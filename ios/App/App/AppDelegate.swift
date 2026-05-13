6a918c713f1b20f0e69f652a917da0de7941198f
Joris Falter <joris@jorisfalter.com>
2026-05-13 09:23:24 +0200

    Disable iOS shake to undo

--- ios/App/App/AppDelegate.swift
+++ ios/App/App/AppDelegate.swift
@@ -8,6 +8,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        application.applicationSupportsShakeToEdit = false
         clearWebViewData()
         return true
     }
